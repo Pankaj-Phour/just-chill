cb5d401fee4b6d535780bf131cca077601a12641
Pankaj Phour <pankaj.phour70@gmail.com>
2023-08-23 15:38:24 +0530

    DAncing girl gif added

--- src/App.js
+++ src/App.js
@@ -2,18 +2,6 @@ import logo from './logo.svg';
 import champagne from './champagne.png';
 import justChill from './juschill.mp3';
 import './App.css';
-<<<<<<< Updated upstream
-
-function App() {
-  return (
-    <div className="App">
-      <div className='music'>
-      <audio controls>
-  <source src={justChill} type="audio/ogg" />
-  <source src={justChill} type="audio/mpeg" />
-  Your browser does not support the audio tag.
-</audio>
-=======
 import dancingGirl from './dancinggirl.gif';
 
 function App() {
@@ -29,7 +17,6 @@ function App() {
       {/* <audio controls autoPlay id="music">
   <source src={justChill} type="audio/mp3" />
 </audio> */}
->>>>>>> Stashed changes
       </div>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
@@ -37,13 +24,10 @@ function App() {
         <img src={champagne} className="App-bottle2" alt="logo" />
         <img src={champagne} className="App-bottle3" alt="logo" />
         <img src={champagne} className="App-bottle4" alt="logo" />
-<<<<<<< Updated upstream
-=======
         <img src={dancingGirl} className="App-dancinggirl1" alt="logo" />
         <img src={dancingGirl} className="App-dancinggirl2" alt="logo" />
         <img src={dancingGirl} className="App-dancinggirl3" alt="logo" />
         <img src={dancingGirl} className="App-dancinggirl4" alt="logo" />
->>>>>>> Stashed changes
         
         <p>
           Just chill <code>src/App.js</code> Just chill.
